b97fafc8d3238898b4c434af3e00b46ffb71ed0c
Ricardo Pizano <ricardo.p@iw.digital>
2017-08-10 13:48:19 -0500

    se agrego el llenado de los departamentos en departamentos

--- src/sample/ControllerDepartamentos.java
+++ src/sample/ControllerDepartamentos.java
@@ -1,4 +1,35 @@
 package sample;
 
-public class ControllerDepartamentos {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class ControllerDepartamentos implements Initializable{
+
+    // Declaracion de objetos y listas de onjetos
+    private DB db;
+    private ArrayList<Departamento> departamentos;
+
+    @FXML
+    private ListView<String> listDepartamentos;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        db = new DB();
+        assert listDepartamentos != null : "fx:id=listDepartamentos";
+        llenarList();
+    }
+
+    private void llenarList() {
+        departamentos = db.getDepartamentos();
+        listDepartamentos.getItems().clear();
+        for(Departamento d : departamentos) {
+            listDepartamentos.getItems().add(d.getNombre() + " - " + d.getResponsable());
+        }
+    }
+
 }
