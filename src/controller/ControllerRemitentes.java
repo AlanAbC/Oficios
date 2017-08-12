17097275c2fde930d44c26c14fbd70c6b4b9f8a6
alana <alanjiraya77@gmail.com>
2017-08-12 10:30:01 -0500

    Se completo remitentes

--- src/controller/ControllerRemitentes.java
+++ src/controller/ControllerRemitentes.java
@@ -1,9 +1,12 @@
 package controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import main.Main;
+import model.Departamento;
 import model.Remitente;
 
 import java.net.URL;
@@ -16,6 +19,7 @@ public class ControllerRemitentes implements Initializable {
     //Declaracion de objetos
     //Arraylist Remitentes
     private ArrayList<Remitente> remitentes;
+    private Remitente remitenteEditar;
 
     @FXML
     private ListView<String> listRemitentes;
@@ -43,7 +47,7 @@ public class ControllerRemitentes implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        assert listRemitentes != null : "fx:id=listDepartamentos";
+        assert listRemitentes != null : "fx:id=listRemitentes";
         assert nombreNuevo != null : "fx:id=nombreNuevo";
         assert encargadoNuevo != null : "fx:id=encargadoNuevo";
         assert nombreEditar != null : "fx:id=nombreEditar";
@@ -51,12 +55,11 @@ public class ControllerRemitentes implements Initializable {
         assert agregar != null : "fx:id=agregar";
         assert eliminar != null : "fx:id=eliminar";
         assert editar != null : "fx:id=editar";
-
-        //Funcion para llenar listview
+        remitenteEditar = new Remitente();
+        desactivarEditarEliminar();
         llenarList();
-
-        //Funcion para asignar todos los listener a los botones
         listenersBotones();
+        listenerListView();
     }
 
     //Funcion para asignar todos los listener a los botones
@@ -70,10 +73,10 @@ public class ControllerRemitentes implements Initializable {
 
             // Comprobaciones de campos vacios
             if(nombreNuevo.getText().equals("")){
-                alert.setContentText("Falta ingresar el nombre del departamento");
+                alert.setContentText("Falta ingresar el nombre del remitente");
                 alert.showAndWait();
             }else if(encargadoNuevo.getText().equals("")){
-                alert.setContentText("Falta ingresar un responsable del departamento");
+                alert.setContentText("Falta ingresar un responsable del remitente");
                 alert.showAndWait();
             }else{
                 // Creacion del nuevo objeto de departamento
@@ -90,7 +93,7 @@ public class ControllerRemitentes implements Initializable {
                     Alert correcto = new Alert(Alert.AlertType.CONFIRMATION);
                     correcto.setTitle("Correcto");
                     correcto.setHeaderText("Operacion correcta");
-                    correcto.setContentText("Se guardo correctamente el remitentes");
+                    correcto.setContentText("Se guardo correctamente el remitente");
                     correcto.showAndWait();
 
                     // Limpiar campos del formulario
@@ -109,76 +112,157 @@ public class ControllerRemitentes implements Initializable {
                 }
             }
         });
-
         editar.setOnAction(event -> {
-
             // Creacion de alerta para campos faltantes
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setTitle("Advertencia");
             alert.setHeaderText("Datos faltantes");
 
+            // Comprobaciones de campos vacios
+            if(nombreEditar.getText().equals("")){
+                alert.setContentText("Falta ingresar el nuevo nombre del remitente");
+                alert.showAndWait();
+            }else if(encargadoEditar.getText().equals("")){
+                alert.setContentText("Falta ingresar un nuevo responsable del remitente");
+                alert.showAndWait();
+            }else{
+                //Creacion de alerta para confirmacion de accion
+                Alert alertc = new Alert(Alert.AlertType.CONFIRMATION);
+                alertc.setTitle("Confirmacion");
+                alertc.setHeaderText("Confirmación de actualización");
+                alertc.setContentText("¿Seguro que deseas modificar el remiente " + remitenteEditar.getNombre() + "?");
+
+                Optional<ButtonType> result =alertc.showAndWait();
+
+                if(result.get() == ButtonType.OK) {
+
+                    // Creacion del nuevo objeto de departamento
+                    Remitente editarRem = new Remitente();
+                    editarRem.setId(remitenteEditar.getId());
+                    editarRem.setNombre(nombreEditar.getText());
+                    editarRem.setResponsable(encargadoEditar.getText());
+
+                    // Insercion del nuevo departamento
+                    String respuesta = Main.db.updateRemitentes(editarRem);
+
+                    // Comprobar registro correcto
+                    if (respuesta.equals("1")) {
+
+                        //Mensaje de insersion correcta
+                        Alert correcto = new Alert(Alert.AlertType.INFORMATION);
+                        correcto.setTitle("Correcto");
+                        correcto.setHeaderText("Operacion correcta");
+                        correcto.setContentText("Se actualizó correctamente el remitente");
+                        correcto.showAndWait();
+
+                        // Limpiar campos del formulario
+                        nombreEditar.setText("");
+                        encargadoEditar.setText("");
+                        editar.setDisable(true);
+                        eliminar.setDisable(true);
+                        nombreEditar.setDisable(true);
+                        encargadoEditar.setDisable(true);
+
+                        // Rellenar listview
+                        llenarList();
+                    } else {
+                        // Mensaje de error
+                        Alert error = new Alert(Alert.AlertType.ERROR);
+                        error.setTitle("Error");
+                        error.setHeaderText("No se pudo actualizar el remitente");
+                        error.setContentText("Error: " + respuesta);
+                        error.showAndWait();
+                    }
+                }
+            }
+        });
+        eliminar.setOnAction(event -> {
             //Creacion de alerta para confirmacion de accion
             Alert alertc = new Alert(Alert.AlertType.CONFIRMATION);
             alertc.setTitle("Confirmacion");
             alertc.setHeaderText("Confirmación de actualización");
-            alertc.setContentText("¿Seguro que deseas modificar este elemento?");
+            alertc.setContentText("¿Seguro que deseas eliminar el remitente " + remitenteEditar.getNombre() + "?");
 
-            // Comprobaciones de campos vacios
-            if(nombreEditar.getText().equals("") && encargadoEditar.getText().equals("")){
-                alert.setContentText("Falta ingresar algun dato");
-                alert.showAndWait();
-            }else{
-                // Creacion del nuevo objeto de remitente
-                Remitente nuevoRem = new Remitente();
-                nuevoRem.setNombre(nombreEditar.getText());
-                nuevoRem.setResponsable(encargadoEditar.getText());
+            Optional<ButtonType> result =alertc.showAndWait();
 
-                //Despliegue de alerta de confirmacion
-                Optional<ButtonType> result =alertc.showAndWait();
-                if(result.get() == ButtonType.OK){
-                    // Insercion del update de remitente
-                    String respuesta = Main.db.updateRemitentes(nuevoRem);
+            if(result.get() == ButtonType.OK){
+                String respuesta = Main.db.deleteRemitente(remitenteEditar.getId());
 
-                    // Comprobar registro correcto
-                    if(respuesta.equals("1")){
-                        //Mensaje de insersion correcta
-                        Alert correcto = new Alert(Alert.AlertType.CONFIRMATION);
-                        correcto.setTitle("Correcto");
-                        correcto.setHeaderText("Operacion correcta");
-                        correcto.setContentText("Se guardo correctamente el remitentes");
-                        correcto.showAndWait();
+                // Comprobar registro correcto
+                if(respuesta.equals("1")){
 
-                        // Limpiar campos del formulario
-                        nombreNuevo.setText("");
-                        encargadoNuevo.setText("");
+                    //Mensaje de insersion correcta
+                    Alert correcto = new Alert(Alert.AlertType.INFORMATION);
+                    correcto.setTitle("Correcto");
+                    correcto.setHeaderText("Operacion correcta");
+                    correcto.setContentText("Se Eliminó correctamente el remitente");
+                    correcto.showAndWait();
 
-                        // Rellenar listview
-                        llenarList();
-                    }else{
-                        // Mensaje de error
-                        Alert error = new Alert(Alert.AlertType.ERROR);
-                        error.setTitle("Error");
-                        error.setHeaderText("No se pudo guardar el remitente");
-                        error.setContentText("Error: " + respuesta);
-                        error.showAndWait();
-                    }
+                    // Limpiar campos del formulario
+                    nombreEditar.setText("");
+                    encargadoEditar.setText("");
+                    editar.setDisable(true);
+                    eliminar.setDisable(true);
+                    nombreEditar.setDisable(true);
+                    encargadoEditar.setDisable(true);
+
+                    // Rellenar listview
+                    llenarList();
                 }else{
-                    //Aqui se renderizara la pagina como se muestra en un inicio
-                    nombreNuevo.setText("");
-                    encargadoNuevo.setText("");
-
+                    // Mensaje de error
+                    Alert error = new Alert(Alert.AlertType.ERROR);
+                    error.setTitle("Error");
+                    error.setHeaderText("No se pudo eliminar el remitente");
+                    error.setContentText("Error: " + respuesta);
+                    error.showAndWait();
                 }
 
             }
         });
     }
 
-    //Funcion para llenar listview
+    private void listenerListView(){
+        listRemitentes.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(newValue != null) {
+                    int index = listRemitentes.getSelectionModel().getSelectedIndex();
+                    if (index >= 0) {
+                        if (remitentes.size() > 0) {
+                            remitenteEditar= remitentes.get(index);
+                            editarELiminar(remitenteEditar);
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private void editarELiminar(Remitente rem){
+        activarEditarEliminar();
+        nombreEditar.setText(rem.getNombre());
+        encargadoEditar.setText(rem.getResponsable());
+    }
+
     private void llenarList() {
         remitentes = Main.db.getRemitentes();
         listRemitentes.getItems().clear();
         for(Remitente r : remitentes) {
-            listRemitentes.getItems().add(r.getNombre() + " - " + r.getResponsable());
+            listRemitentes.getItems().add("Nombre: " + r.getNombre() + "\nResponsable: " + r.getResponsable());
         }
+    }
+
+    private void desactivarEditarEliminar(){
+        editar.setDisable(true);
+        eliminar.setDisable(true);
+        nombreEditar.setDisable(true);
+        encargadoEditar.setDisable(true);
+    }
+
+    private void activarEditarEliminar(){
+        editar.setDisable(false);
+        eliminar.setDisable(false);
+        nombreEditar.setDisable(false);
+        encargadoEditar.setDisable(false);
     }
 }
